e2415911af9a50c010547f37d91e1ff06d083d28
Achyls Luna <jomasenriquez2004@gmail.com>
2025-02-24 15:48:10 +0800

    updated

--- Mobile-API/type.php
+++ Mobile-API/type.php
@@ -1,18 +1,11 @@
 <?php
-// Define duty statuses
-define('DUTY_STATUS_OFFICE', 'Office Duty');
-define('DUTY_STATUS_FACILITATOR', 'Student Facilitator');
-define('DUTY_STATUS_HK_BBS', 'HK BBS');
+
 
 // Define scholarship types
 define('SCHOLARSHIP_TYPE_25', '25%');
 define('SCHOLARSHIP_TYPE_50', '50%');
 define('SCHOLARSHIP_TYPE_75', '75%');
 
-// Define courses
-define('COURSE_CITE', 'CITE');
-define('COURSE_CMA', 'CMA');
-define('COURSE_CRIM', 'CRIM');
 
 // Define year levels
 define('YEAR_FIRST', 'First Year');
