564a8805ad7ee578318399cdef1edb29e595a3da
Achyls Luna <jomasenriquez2004@gmail.com>
2025-03-09 22:06:42 +0800

    crashingg

--- app/src/main/java/Model/DutyLogItem.kt
+++ app/src/main/java/Model/DutyLogItem.kt
@@ -4,9 +4,9 @@ import com.google.gson.annotations.SerializedName
 
 data class DutyLogItem(
     @SerializedName("user_id") val userId: Int,
-    @SerializedName("duty_date") val dutyDate: String,
-    @SerializedName("time_in") val timeIn: String,
-    @SerializedName("time_out") val timeOut: String,
+    @SerializedName("duty_date") val duty_date: String,
+    @SerializedName("time_in") val time_in: String,
+    @SerializedName("time_out") val time_out: String,
     @SerializedName("duration") val duration: String,
     @SerializedName("status") val status: String
 )
--- app/src/main/java/com/example/scholarly/SuccessActivity.kt
+++ app/src/main/java/com/example/scholarly/SuccessActivity.kt
@@ -11,7 +11,7 @@ class SuccessActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_success)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.MainActivity)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
